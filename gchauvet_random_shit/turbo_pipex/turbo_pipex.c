0ff74047927673f1dd350dc86540963d6e8f7983
AngelKhos <aurorethomas95@gmail.com>
2025-05-09 17:24:50 +0200

    update here_doc

--- gchauvet_random_shit/turbo_pipex/turbo_pipex.c
+++ gchauvet_random_shit/turbo_pipex/turbo_pipex.c
@@ -6,7 +6,7 @@
 /*   By: authomas <authomas@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/02 16:00:31 by gchauvet          #+#    #+#             */
-/*   Updated: 2025/05/09 16:40:56 by authomas         ###   ########.fr       */
+/*   Updated: 2025/05/09 17:23:09 by authomas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,25 @@
 #include <sys/wait.h>
 #include "turbo_pipex.h"
 
+char	*convert_file_to_str(int file)
+{
+	char	*str;
+	char	*line;
+
+	line = get_next_line(file)
+	while (line)
+	{
+		ft_strjoin(str, line);
+		free(line)
+		line = get_next_line(file);
+	}
+	return (str);
+}
+
 void	here_doc(char *argv)
 {
 	char	*end_message = "fin";
-	char	*line;
+	char	*here_doc_str;
 	int	pipes[2];
 	int	pid;
 	int here_doc;
@@ -32,8 +47,13 @@ void	here_doc(char *argv)
 	if (pid == 0)
 	{
 		here_doc = open(".tmp_here_doc", O_WRONLY | O_CREAT | O_TRUNC, 0777);
+		dup2(here_doc, STDOUT_FILENO);
+		here_doc_str = convert_file_to_str(here_doc);
+		while (ft_strncmp(here_doc_str, end_message, ft_strlen(end_message)) == 0)
+		{
+			here_doc_str = convert_file_to_str(here_doc);
+		}
 	}
-	pud2()
 }
 
 void	do_fork(char *argv, char **env)
