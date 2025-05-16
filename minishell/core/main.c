9374394555897de8e6807343ac7a418a311ae1a0
Gael Chauvet <gchauvet@z4r10p3.42lyon.fr>
2025-05-16 15:52:42 +0200

    update main

--- minishell/core/main.c
+++ minishell/core/main.c
@@ -6,11 +6,12 @@
 /*   By: gchauvet <gchauvet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/14 14:53:42 by gchauvet          #+#    #+#             */
-/*   Updated: 2025/05/16 15:20:30 by gchauvet         ###   ########.fr       */
+/*   Updated: 2025/05/16 15:51:19 by gchauvet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft/libft.h"
+#include "../include/data.h"
 #include <stdlib.h>
 #include <readline/readline.h>
 #include <readline/history.h>
@@ -18,18 +19,25 @@
 int main(int argc, char **argv, char **envp)
 {
 	char	*input;
+	t_data	*data;
+	
 	(void)argc;
 	(void)argv;
 	(void)envp;
+	data = malloc(sizeof(t_data));
 	while (1)
 	{
-		input = readline("prompte.png>");
+		input = readline("prompt.png>");
 		if (input)
 		{
-			add_history(input);
 			ft_printf("%s", input);
 			if (ft_strlen(input) >= 1)
+			{
+				add_history(input);
+				//pars(data, input);
+				//exec(data);
 				ft_printf("\n");
+			}
 			free(input);
 		}
 	}
