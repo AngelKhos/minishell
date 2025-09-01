303d09fce3a137d563a96ab9a212de2047d6dc8d
Gael Chauvet <gchauvet@z3r3p3.42lyon.fr>
2025-09-01 11:35:04 +0200

    fix multiple display of the prompt for SIGINT

--- minishell/core/signal.c
+++ minishell/core/signal.c
@@ -6,7 +6,7 @@
 /*   By: gchauvet <gchauvet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/16 16:09:19 by gchauvet          #+#    #+#             */
-/*   Updated: 2025/08/20 11:37:49 by gchauvet         ###   ########.fr       */
+/*   Updated: 2025/08/30 14:44:22 by gchauvet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,13 @@
 void	sigint_handle(int code)
 {
 	(void)code;
-	ft_printf("\n");
-	rl_replace_line("", 0);
-	rl_on_new_line();
 	if (g_pid == 0)
+	{
+		ft_printf("\n");
+		rl_replace_line("", 0);
+		rl_on_new_line();
 		rl_redisplay();
+	}
 }
 
 void	handle_signal(void)
