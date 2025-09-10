b4b62f7d43850eb91b51c69f1d566a73645d2bc4
Gael Chauvet <gchauvet@z4r10p4.42lyon.fr>
2025-09-10 17:29:51 +0200

    rework export

--- minishell/builtins/export.c
+++ minishell/builtins/export.c
@@ -3,32 +3,15 @@
 /*                                                        :::      ::::::::   */
 /*   export.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: authomas <authomas@student.42lyon.fr>      +#+  +:+       +#+        */
+/*   By: gchauvet <gchauvet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/21 13:14:18 by gchauvet          #+#    #+#             */
-/*   Updated: 2025/09/10 15:29:16 by authomas         ###   ########lyon.fr   */
+/*   Updated: 2025/09/10 17:27:33 by gchauvet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/data.h"
 #include "../libft/libft.h"
-
-int	export_part_3(t_cmd *cmd, t_env *leaf, char **val)
-{
-	if (val[1])
-	{
-		leaf->data.value = ft_strdup(val[1]);
-		if (!leaf->data.value)
-			return (free(leaf->data.value), free(leaf), 1);
-	}
-	else if (ft_strchr(cmd->parts[1].str, '='))
-	{
-		leaf->data.value = ft_strdup("");
-		if (!leaf->data.value)
-			return (free(leaf->data.value), free(leaf), 1);
-	}
-	return (0);
-}
 
 int	ft_isok(char *str)
 {
@@ -57,41 +40,32 @@ int	ft_isok(char *str)
 	return (1);
 }
 
-int	export_part_2(t_env *leaf, char **val, t_cmd *cmd)
+int	export_part_2(t_env *leaf)
 {
 	if (!leaf->data.key)
 		return (free(leaf), 1);
-	if (ft_isok(val[0]) == 0)
-		return (free(leaf->data.key), free(leaf), free_array(val), 1);
-	leaf->data.value = NULL;
-	if (export_part_3(cmd, leaf, val) == 1)
-		return (1);
+	if (ft_isok(leaf->data.key) == 0)
+		return (tree_delete_node(leaf), 1);
 	return (0);
 }
 
 int	export_core(t_data *data, t_cmd *cmd, int part_i)
 {
 	t_env	*leaf;
-	char	**val;
 	int		code;
 
-	leaf = ft_calloc(sizeof(t_env), 1);
+	leaf = env_parse_node(cmd->parts[part_i].str);
 	if (!leaf)
 		return (1);
-	val = ft_split(cmd->parts[part_i].str, '=');
-	if (!val)
-		return (free(leaf), 1);
-	leaf->data.key = ft_strdup(val[0]);
 	if (!leaf->data.key)
-		return (free(leaf), free_array(val), 1);
-	code = export_part_2(leaf, val, cmd);
+		return (tree_delete_node(leaf), 1);
+	code = export_part_2(leaf);
 	if (code == 1)
 		return (code);
 	if (data->env)
 		tree_insert(data->env, leaf);
 	else
 		data->env = leaf;
-	free_array(val);
 	return (0);
 }
 
